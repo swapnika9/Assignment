8f643cc8eb6aa05dfb2ac34a1768e36b5c0e8365
swapnika <telagathotiswapnika9@gmail.com>
2021-06-06 13:07:41 +0530

    'first'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home' , component:HomeComponent},
+  {path:'products' , component:ProductsComponent},
+  {path:'services' , component:ServicesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
